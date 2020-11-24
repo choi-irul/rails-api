257bb0994802811d38cd36a4dd1f58a2cb036ace
choirul <choirul.sea@gmail.com>
2020-11-24 10:07:24 +0700

    update

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,17 +2,10 @@
 
 module Api
   class ProductsController < ApiController
-    # before_action :authorize_request
+    before_action :authorize_request
 
     def index
       @products = Product.eager_load(:store, :variants).search(params)
-    end
-
-    def self.generate_number(is_pro = false)
-      initial = is_pro ? 'PA' : 'SO'
-      number = format(initial + '%s%s', Date.today.strftime('%y%m%d'), SecureRandom.hex.slice(0, 4).upcase)
-      number.upcase!
-      find_by(number: number) ? generate_number : number
     end
   end
 end
