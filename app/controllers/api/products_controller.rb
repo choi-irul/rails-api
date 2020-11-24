a0ac5f956a67a500fbb402554405bee14689abad
choirul <choirul.sea@gmail.com>
2020-11-24 10:09:16 +0700

    update

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,7 +2,7 @@
 
 module Api
   class ProductsController < ApiController
-    before_action :authorize_request
+    # before_action :authorize_request
 
     def index
       @products = Product.eager_load(:store, :variants).search(params)
